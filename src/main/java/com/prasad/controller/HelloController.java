0ec661c494720aa29343d3419dda190bb1126a9f
Ramprasad V <vrprasad@gmail.com>
2022-07-23 15:09:29 +0530

    Initial commit

---
+++ src/main/java/com/prasad/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.prasad.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import  org.springframework.web.bind.annotation.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<String> healthcheck() {
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String responseText = "hello-api Healthcheck @ "+ timeStamp+" - All OK";
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.TEXT_PLAIN).body(responseText);
+    }
+
+
+
+}
